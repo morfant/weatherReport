1f0ec71e3e0a96739772f4eeb44d77a644e6b536
Gangil <git>
2016-07-11 11:58:57 +0900

    Add kadira, winston, winston-papertrail, ostriologger

--- client/templates/pages/strplayer.js
+++ client/templates/pages/strplayer.js
@@ -1,6 +1,8 @@
 
 Template.strplayer.helpers({
-
+    strCanBePlaying: function() {
+        console.log("str can be playing");
+    }
 });
 
 Template.strplayer.events({
@@ -20,3 +22,47 @@ Template.strplayer.events({
 
   
 });
+
+
+Template.strplayer.rendered = function() {
+    console.log("strplayer rendered");
+    var playBut = document.getElementById("playButton");
+    var audio = document.getElementById('audio');
+
+    // console.log(audio.networkState);
+    // console.log(audio.duration);
+    // console.log(audio.readyState);
+    // console.log(audio.ended);
+    // console.log(audio.currentSrc);
+    // console.log(audio.error);
+
+    // var httpCall = HTTP.call("GET",
+    //     "http://jjwc.cafe24.com:8000/mpd.mp3",
+    //     {},
+    //     function(error, result){
+    //         if (error){
+    //             console.log("HTTP call has error!");
+    //             console.log(error);
+    //         }else{
+    //             console.log(result.statusCode);
+    //         }
+    // });
+    Meteor.call('checkSrc', "http://jjwc.cafe24.com:8000/mpd.mp3", function(error, result){
+        console.log(result);
+    });
+ 
+
+    if (audio.networkState !== HTMLMediaElement.NETWORK_LOADING){
+        if (playBut.disabled == false){
+            playBut.disabled = true;
+        }
+    } else {
+        if (playBut.disabled == true){
+            playBut.disabled = false;
+        }       
+    }
+
+    console.log("button disabled: " + playBut.disabled);
+
+};
+
