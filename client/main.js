d18b7358627dc464ac05a72e4c37b0fb58d585d7
Gangil <git>
2016-07-13 18:45:21 +0900

    making Posting part

--- client/main.js
+++ client/main.js
@@ -1,10 +1,5 @@
 var streamURL = 'http://jjwc.cafe24.com:8000/mpd.ogg';
 
-Template.main.helpers({
-	streamLink: streamURL,
-	streamTitle: 'Test title - onNow'
-
-});
 
 
 // Template.upNext.helpers({
--- server/fixture.js
+++ server/fixture.js
@@ -1,16 +1,25 @@
 if (Posts.find().count() === 0) {
   Posts.insert({
     title: '#1',
-    streamURL: 'http://jjwc.cafe24.com:8000/mpd.ogg'
+    contents: 'hello',
+    isOnNow: true,
+    isUpNext: false,
+    isLasts: false
   });
 
   Posts.insert({
     title: '#2',
-    streamURL: 'http://jjwc.cafe24.com:8000/mpd.ogg'
+    contents: 'http://jjwc.cafe24.com:8000/mpd.ogg',
+    isOnNow: false,
+    isUpNext: true,
+    isLasts: false
   });
 
   Posts.insert({
     title: '#3',
-    streamURL: 'http://jjwc.cafe24.com:8000/mpd.ogg'
+    contents: 'http://jjwc.cafe24.com:8000/mpd.ogg',
+    isOnNow: false,
+    isUpNext: false,
+    isLasts: true 
   });
 }
